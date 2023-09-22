4e0c4b7b6deaea2012f806e4f4d547f97773ac77
Gareth Thomas <thomasgareth3@gmail.com>
2023-09-22 16:05:17 +0200

    feat:card js and css

--- script.js
+++ script.js
@@ -18,12 +18,46 @@ recipes.forEach((recipe) => {
   cardTitle.className = "card-title";
   cardTitle.textContent = recipe.name;
 
+  const cardDescriptionTitle = document.createElement("p");
+  cardDescriptionTitle.className = "card-subsection-title";
+  cardDescriptionTitle.textContent = "Recette";
+
   const cardDescription = document.createElement("p");
   cardDescription.className = "card-text";
   cardDescription.textContent = recipe.description;
 
+  const cardIngredientsTitle = document.createElement("p");
+  cardIngredientsTitle.className = "card-subsection-title";
+  cardIngredientsTitle.textContent = "Ingredients";
+
+  const cardIngredients = document.createElement("div");
+  cardIngredients.className = "ingredients-section";
+
+  const ingredients = recipe.ingredients;
+
+  ingredients.forEach((ingredient) => {
+    const ingredientSubgroup = document.createElement("div");
+    ingredientSubgroup.className = "ingredient-subgroup";
+
+    const ingredientName = document.createElement("p");
+    ingredientName.textContent = ingredient.ingredient;
+    ingredientName.className = "ingredient-name";
+    ingredientSubgroup.appendChild(ingredientName);
+
+    const ingredientQuantity = document.createElement("p");
+    ingredientQuantity.textContent = `${ingredient.quantity} ${ingredient.unit}`;
+    ingredientQuantity.className = "ingredient-quantity";
+
+    ingredientSubgroup.appendChild(ingredientQuantity);
+
+    cardIngredients.appendChild(ingredientSubgroup);
+  });
+
   cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardDescriptionTitle);
   cardBody.appendChild(cardDescription);
+  cardBody.appendChild(cardIngredientsTitle);
+  cardBody.appendChild(cardIngredients);
   card.appendChild(cardImage);
   card.appendChild(cardBody);
 
