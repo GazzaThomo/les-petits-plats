138779c61618872cd868ef366b79e5c07a668a6a
Gareth Thomas <thomasgareth3@gmail.com>
2023-09-26 23:45:02 +0200

    feat : dropdown menus

--- script.js
+++ script.js
@@ -1,70 +1,190 @@
 import { recipes } from "./recipes.js";
 
-recipes.forEach((recipe) => {
-  const cardsContainer = document.querySelector(".recipe-cards-section");
-  let card = document.createElement("div");
-  card.className = "card";
-  card.setAttribute("data-id", recipe.id);
+let listOfIngredients = getIngredients();
+let listOfAppliances = getAppareils();
+let listOfUtencils = getUstentiles();
 
-  const cardImage = document.createElement("img");
-  cardImage.className = "card-img-top";
-  cardImage.src = `./assets/images/plats/${recipe.image}`;
-  cardImage.alt = recipe.name;
+function loadPageInitial() {
+  loadRecipeCards();
+  loadIngredientsDropdownInitial(listOfIngredients);
+  loadAppliancesDropdownInitial(listOfAppliances);
+  loadUtensilsDropdownInitial(listOfUtencils);
+}
 
-  const cardTime = document.createElement("p");
-  cardTime.className = "card-time";
-  cardTime.textContent = `${recipe.time}min`;
+function loadRecipeCards() {
+  recipes.forEach((recipe) => {
+    const cardsContainer = document.querySelector(".recipe-cards-section");
+    let card = document.createElement("div");
+    card.className = "card";
+    card.setAttribute("data-id", recipe.id);
 
-  const cardBody = document.createElement("div");
-  cardBody.className = "card-body";
+    const cardImage = document.createElement("img");
+    cardImage.className = "card-img-top";
+    cardImage.src = `./assets/images/plats/${recipe.image}`;
+    cardImage.alt = recipe.name;
 
-  const cardTitle = document.createElement("h5");
-  cardTitle.className = "card-title";
-  cardTitle.textContent = recipe.name;
+    const cardTime = document.createElement("p");
+    cardTime.className = "card-time";
+    cardTime.textContent = `${recipe.time}min`;
 
-  const cardDescriptionTitle = document.createElement("p");
-  cardDescriptionTitle.className = "card-subsection-title";
-  cardDescriptionTitle.textContent = "Recette";
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
 
-  const cardDescription = document.createElement("p");
-  cardDescription.className = "card-text";
-  cardDescription.textContent = recipe.description;
+    const cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = recipe.name;
 
-  const cardIngredientsTitle = document.createElement("p");
-  cardIngredientsTitle.className = "card-subsection-title";
-  cardIngredientsTitle.textContent = "Ingredients";
+    const cardDescriptionTitle = document.createElement("p");
+    cardDescriptionTitle.className = "card-subsection-title";
+    cardDescriptionTitle.textContent = "Recette";
 
-  const cardIngredients = document.createElement("div");
-  cardIngredients.className = "ingredients-section";
+    const cardDescription = document.createElement("p");
+    cardDescription.className = "card-text";
+    cardDescription.textContent = recipe.description;
 
-  const ingredients = recipe.ingredients;
+    const cardIngredientsTitle = document.createElement("p");
+    cardIngredientsTitle.className = "card-subsection-title";
+    cardIngredientsTitle.textContent = "Ingredients";
 
-  ingredients.forEach((ingredient) => {
-    const ingredientSubgroup = document.createElement("div");
-    ingredientSubgroup.className = "ingredient-subgroup";
+    const cardIngredients = document.createElement("div");
+    cardIngredients.className = "ingredients-section";
 
-    const ingredientName = document.createElement("p");
-    ingredientName.textContent = ingredient.ingredient;
-    ingredientName.className = "ingredient-name";
-    ingredientSubgroup.appendChild(ingredientName);
+    const ingredients = recipe.ingredients;
 
-    const ingredientQuantity = document.createElement("p");
-    ingredientQuantity.textContent = `${ingredient.quantity} ${ingredient.unit}`;
-    ingredientQuantity.className = "ingredient-quantity";
+    ingredients.forEach((ingredient) => {
+      const ingredientSubgroup = document.createElement("div");
+      ingredientSubgroup.className = "ingredient-subgroup";
 
-    ingredientSubgroup.appendChild(ingredientQuantity);
+      const ingredientName = document.createElement("p");
+      ingredientName.textContent = ingredient.ingredient;
+      ingredientName.className = "ingredient-name";
+      ingredientSubgroup.appendChild(ingredientName);
 
-    cardIngredients.appendChild(ingredientSubgroup);
+      const ingredientQuantity = document.createElement("p");
+      ingredientQuantity.textContent = `${ingredient.quantity} ${ingredient.unit}`;
+      ingredientQuantity.className = "ingredient-quantity";
+
+      ingredientSubgroup.appendChild(ingredientQuantity);
+
+      cardIngredients.appendChild(ingredientSubgroup);
+    });
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardDescriptionTitle);
+    cardBody.appendChild(cardDescription);
+    cardBody.appendChild(cardIngredientsTitle);
+    cardBody.appendChild(cardIngredients);
+    card.appendChild(cardImage);
+    card.appendChild(cardTime);
+    card.appendChild(cardBody);
+
+    cardsContainer.appendChild(card);
+  });
+}
+
+function getIngredients() {
+  let allIngredients = [];
+  recipes.forEach((recipe) => {
+    let ingredients = recipe.ingredients;
+    ingredients.forEach((ingredient) => {
+      let someIngredient = ingredient.ingredient.trim();
+      allIngredients.push(someIngredient.toLowerCase());
+    });
+  });
+  allIngredients = allIngredients.sort();
+  let setIngredients = [...new Set(allIngredients)];
+
+  //Use this to make the first char of each word uppercase, makes it look nicer
+  let uniqueIngredients = setIngredients.map(
+    (str) => str.charAt(0).toUpperCase() + str.slice(1)
+  );
+  return uniqueIngredients;
+}
+
+function getAppareils() {
+  let allAppareils = [];
+  recipes.forEach((appareil) => {
+    let someAppliance = appareil.appliance.trim();
+    allAppareils.push(someAppliance.toLowerCase());
+  });
+  allAppareils.sort();
+  let setAppareils = [...new Set(allAppareils)];
+
+  let uniqueAppareils = setAppareils.map(
+    (str) => str.charAt(0).toUpperCase() + str.slice(1)
+  );
+  return uniqueAppareils;
+}
+
+// function getUstentiles() {
+//   let allUstentiles = [];
+//   recipes.forEach((ustencile) => {
+//     allUstentiles.push(ustencile.ustensils);
+//   });
+//   allUstentiles.flat();
+//   allUstentiles.forEach((ustencile) => {
+//     ustencile = ustencile.trim().toLowerCase();
+//   });
+//   allUstentiles.sort();
+//   let setUstensiles = [...new Set(allUstentiles)];
+
+//   let uniqueUstensiles = setUstensiles.map(
+//     (str) => str.charAt(0).toUpperCase() + str.slice(1)
+//   );
+//   return(uniqueUstensiles);
+// }
+
+function getUstentiles() {
+  let allUstentiles = [];
+  recipes.forEach((recipe) => {
+    allUstentiles.push(...recipe.ustensils);
   });
 
-  cardBody.appendChild(cardTitle);
-  cardBody.appendChild(cardDescriptionTitle);
-  cardBody.appendChild(cardDescription);
-  cardBody.appendChild(cardIngredientsTitle);
-  cardBody.appendChild(cardIngredients);
-  card.appendChild(cardImage);
-  card.appendChild(cardTime);
-  card.appendChild(cardBody);
+  allUstentiles = allUstentiles.map((ustencile) =>
+    ustencile.trim().toLowerCase()
+  );
 
-  cardsContainer.appendChild(card);
-});
+  let uniqueUstensilesSet = new Set(allUstentiles);
+
+  let uniqueUstensiles = [...uniqueUstensilesSet].map(
+    (str) => str.charAt(0).toUpperCase() + str.slice(1)
+  );
+
+  uniqueUstensiles.sort();
+  return uniqueUstensiles;
+}
+
+function loadIngredientsDropdownInitial(listOfIngredients) {
+  const dropdown = document.querySelector(".dropdown-menu-ingredients");
+
+  listOfIngredients.forEach((ingredient) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-ingredient";
+    listItem.textContent = ingredient;
+    dropdown.appendChild(listItem);
+  });
+}
+
+function loadAppliancesDropdownInitial(listOfAppliances) {
+  const dropdown = document.querySelector(".dropdown-menu-appareils");
+
+  listOfAppliances.forEach((appliance) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-appliance";
+    listItem.textContent = appliance;
+    dropdown.appendChild(listItem);
+  });
+}
+
+function loadUtensilsDropdownInitial(listOfUtencils) {
+  const dropdown = document.querySelector(".dropdown-menu-ustensils");
+
+  listOfUtencils.forEach((utensil) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-utensil";
+    listItem.textContent = utensil;
+    dropdown.appendChild(listItem);
+  });
+}
+
+loadPageInitial();
