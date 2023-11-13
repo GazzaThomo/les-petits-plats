9826a071e51c7676a152532aff0d92c8e7a3c83a
Gareth <thomasgareth3@gmail.com>
2023-11-13 02:09:45 +0100

    add test case files

--- testSpeed/common.js
+++ testSpeed/common.js
@@ -1826,10 +1826,27 @@ const recipes = [
   },
 ];
 
+const testArray = ["Chocolat", "fraise"];
 let recipesCopy = [...recipes];
-let listOfIngredients = getIngredients();
-let listOfAppliances = getAppareils();
-let listOfUtencils = getUstentiles();
+
+function newSearch() {
+  const searchbarElement = document.querySelector(".main-search-bar");
+  const inputWords = getMainSearchbarWords(searchbarElement);
+  const badgeWords = getAllBadgeText();
+  const allSearchWords = [...inputWords, ...badgeWords];
+  const isMalicious = checkForMaliciousInput(allSearchWords);
+
+  if (isMalicious) {
+    console.log("Malicious input attempt !");
+    return;
+  } else {
+    if (inputWords.some((word) => word.length >= 3)) {
+      searchRecipe(allSearchWords);
+    } else {
+      searchRecipe(badgeWords);
+    }
+  }
+}
 
 function checkForMaliciousInput(inputArray) {
   //this pattern checks for the basic sql commands
@@ -1848,150 +1865,6 @@ function addHiddenProperty() {
   recipesCopy.forEach((recipe) => {
     recipe.isHidden = false;
   });
-}
-
-function getIngredients(input = []) {
-  let allIngredients = [];
-  let filteredIngredients;
-  if (input === -1) input = [];
-
-  recipes.forEach((recipe) => {
-    if (!recipe.isHidden) {
-      let object = {};
-      let ingredients = recipe.ingredients;
-      let specificRecipeIngredients = [];
-      object.id = recipe.id;
-      ingredients.forEach((ingredient) => {
-        let someIngredient = ingredient.ingredient.trim();
-        allIngredients.push(someIngredient.toLowerCase());
-        specificRecipeIngredients.push(someIngredient.toLowerCase());
-      });
-      object.ingredients = specificRecipeIngredients;
-    }
-  });
-
-  allIngredients = allIngredients.sort();
-
-  //this creates a unique set of all ingredients
-  let setIngredients = [...new Set(allIngredients)];
-
-  if (input.length > 0) {
-    //transform the input to lowercase for comparison
-    let inputLowerCase = input.map((item) => item.toLowerCase());
-
-    //filter out ingredients that appear in the input. If input is empty array, this does nothing
-    filteredIngredients = setIngredients.filter(
-      (ingredient) => !inputLowerCase.includes(ingredient)
-    );
-  } else {
-    filteredIngredients = setIngredients;
-  }
-
-  // Use this to make the first char of each word uppercase, makes it look nicer
-  const uniqueIngredients = filteredIngredients.map(
-    (str) => str.charAt(0).toUpperCase() + str.slice(1)
-  );
-
-  globalIngredients = uniqueIngredients;
-  return uniqueIngredients;
-}
-
-function getAppareils(input = []) {
-  let allAppareils = [];
-  let filteredAppliances;
-  recipes.forEach((appareil) => {
-    if (!appareil.isHidden) {
-      let someAppliance = appareil.appliance.trim();
-      allAppareils.push(someAppliance.toLowerCase());
-    }
-  });
-  allAppareils.sort();
-  let setAppareils = [...new Set(allAppareils)];
-
-  if (input.length > 0) {
-    let inputLowerCase = input.map((item) => item.toLowerCase());
-
-    filteredAppliances = setAppareils.filter(
-      (appliance) => !inputLowerCase.includes(appliance)
-    );
-  } else {
-    filteredAppliances = setAppareils;
-  }
-
-  const uniqueAppareils = filteredAppliances.map(
-    (str) => str.charAt(0).toUpperCase() + str.slice(1)
-  );
-  globalAppliances = uniqueAppareils;
-
-  return uniqueAppareils;
-}
-
-function getUstentiles(input = []) {
-  let allUstentiles = [];
-  let filteredUtensils;
-  recipes.forEach((recipe) => {
-    if (!recipe.isHidden) {
-      allUstentiles.push(...recipe.ustensils);
-    }
-  });
-
-  allUstentiles.sort();
-  allUstentiles = allUstentiles.map((ustencile) =>
-    ustencile.trim().toLowerCase()
-  );
-
-  let uniqueUstensilesSet = [...new Set(allUstentiles)];
-
-  if (input.length > 0) {
-    let inputLowerCase = input.map((item) => item.toLowerCase());
-
-    filteredUtensils = uniqueUstensilesSet.filter(
-      (utensil) => !inputLowerCase.includes(utensil)
-    );
-  } else {
-    filteredUtensils = uniqueUstensilesSet;
-  }
-  const uniqueUstensiles = [...filteredUtensils].map(
-    (str) => str.charAt(0).toUpperCase() + str.slice(1)
-  );
-  globalUtensils = uniqueUstensiles;
-
-  return uniqueUstensiles;
-}
-
-function searchRecipe(arrayOfStrings) {
-  // arrayOfStrings.some((word) => word.length >= 3)
-  if (arrayOfStrings.length > 0) {
-    changeIsHiddenProperty(arrayOfStrings);
-    hideCards();
-    reloadDropdownsOnMainSearch(arrayOfStrings);
-    updateRecipeCountText();
-  } else {
-    changeIsHiddenProperty([]);
-    hideCards(-1);
-    reloadDropdownsOnMainSearch(-1);
-    updateRecipeCountText(-1);
-  }
-}
-
-function changeIsHiddenProperty(words) {
-  if (words.length !== 0) {
-    recipesCopy.forEach((recipe) => {
-      const wordsArePresent = words.every((word) => wordInRecipe(word, recipe));
-      if (!wordsArePresent) {
-        // console.log(recipe.name + " is hidden");
-        recipe.isHidden = true;
-      } else {
-        // console.log(recipe.name + " is NOT hidden");
-
-        recipe.isHidden = false;
-      }
-    });
-  } else {
-    recipesCopy.forEach((recipe) => {
-      recipe.isHidden = false;
-    });
-  }
 }
 
 function wordInRecipe(word, recipe) {
@@ -2020,121 +1893,11 @@ function wordInRecipe(word, recipe) {
   return true;
 }
 
-function hideCards(input) {
-  const recipeCardsSection = document.querySelector(".recipe-cards-section");
-  const cards = document.querySelectorAll(".card");
-  const noCorrespondanceElement = document.querySelector(
-    ".no-correspondance-message"
-  );
-
-  recipeCardsSection.classList.add("hide");
-  setTimeout(() => {
-    if (input === -1) {
-      cards.forEach((card) => {
-        card.style.display = "";
-      });
-    } else {
-      cards.forEach((card) => {
-        const dataId = parseInt(card.getAttribute("data-id"), 10);
-
-        // find the corresponding recipe in recipesCopy
-        const correspondingRecipe = recipesCopy.find(
-          (recipe) => recipe.id === dataId
-        );
-
-        // Check if the recipe was found and then check the isHidden property
-        if (correspondingRecipe) {
-          if (correspondingRecipe.isHidden) {
-            card.style.display = "none";
-          } else {
-            card.style.display = "block";
-          }
-        } else {
-          noCorrespondanceElement.style.display = "block";
-        }
-      });
-    }
-    recipeCardsSection.classList.remove("hide");
-  }, 500);
-}
-
-// helper functions for the other js files
-function getMainSearchbarWords(inputElement) {
-  const input = inputElement.value.toLowerCase();
-  const inputWords = input
-    .trim()
-    .split(" ")
-    .filter((word) => word.length > 0);
-
-  return inputWords;
-}
-
-function getAllBadgeText() {
-  const badges = document.querySelectorAll(".badge-text");
-  let badgeText = [];
-  badges.forEach((badge) => {
-    badgeText.push(badge.textContent);
-  });
-
-  return badgeText;
-}
-
-function reloadDropdownsOnMainSearch(input) {
-  const dropdownIngredientList = document.querySelectorAll(".list-ingredient");
-  const dropdownApplianceList = document.querySelectorAll(".list-appliance");
-  const dropdownUtensilList = document.querySelectorAll(".list-utensil");
-
-  const newIngredientList = getIngredients(input);
-  const newApplianceList = getAppareils(input);
-  const newUtensilList = getUstentiles(input);
-
-  if (input === -1) {
-    dropdownIngredientList.forEach((item) => {
-      item.style.display = "";
-    });
-
-    dropdownApplianceList.forEach((item) => {
-      item.style.display = "";
-    });
-
-    dropdownUtensilList.forEach((item) => {
-      item.style.display = "";
-    });
-    return;
-  }
-
-  dropdownIngredientList.forEach((item) => {
-    const value = item.textContent.toLowerCase();
-
-    // inputWords.some will go over the array, taking each word, then we wheck to see if that word is included in value
-    const isMatched = newIngredientList.some((word) =>
-      value.includes(word.toLowerCase())
-    );
-    hideListItemsInDropdowns(isMatched, item);
-  });
-
-  dropdownApplianceList.forEach((item) => {
-    const value = item.textContent.toLowerCase();
-    const isMatched = newApplianceList.some((word) =>
-      value.includes(word.toLowerCase())
-    );
-    hideListItemsInDropdowns(isMatched, item);
-  });
-
-  dropdownUtensilList.forEach((item) => {
-    const value = item.textContent.toLowerCase();
-    const isMatched = newUtensilList.some((word) =>
-      value.includes(word.toLowerCase())
-    );
-    hideListItemsInDropdowns(isMatched, item);
-  });
-}
-
-function hideListItemsInDropdowns(match, item) {
-  if (!match) {
-    item.style.display = "none";
+function hideListItemsInDropdowns(isIncluded, item) {
+  if (isIncluded) {
+    item.style.display = "";
   } else {
-    item.style.display = "block";
+    item.style.display = "none";
   }
 }
 
@@ -2156,3 +1919,23 @@ function updateRecipeCountText(input) {
     textElement.textContent = newText;
   }
 }
+
+function searchRecipe(arrayOfStrings) {
+  if (arrayOfStrings.length > 0) {
+    changeIsHiddenProperty(arrayOfStrings);
+    hideCards();
+    reloadDropdownsOnMainSearch(arrayOfStrings);
+    updateRecipeCountText();
+  } else {
+    changeIsHiddenProperty([]);
+    hideCards(-1);
+    reloadDropdownsOnMainSearch(-1);
+    updateRecipeCountText(-1);
+  }
+}
+
+function init() {
+  addHiddenProperty();
+}
+
+init();
